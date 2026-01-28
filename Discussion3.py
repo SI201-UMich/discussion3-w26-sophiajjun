bf008e447b74d6ebc26656f0b69544ff0694809a
Sophia Jun <sophiajun@MacBook-Air-1161.local>
2026-01-28 17:40:20 -0500

    code

--- Discussion3.py
+++ Discussion3.py
@@ -3,18 +3,19 @@ import math
 class Rectangle():
     # Create the constructor "__init__" method
 
-    # YOUR CODE HERE
-
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
 
     # Create the "__str__" method
 
-    # YOUR CODE HERE
-
-
+    def __str__(self):
+        return f"A rectangle with width {self.width} and height {self.height}"
 
     # Create the "area_calculator" method
 
+    def area_calculator
     # YOUR CODE HERE
 
 
